078ec9220725e336ce9d007ce732f163f9a339fd
karuna24s <karuna24s@gmail.com>
2016-07-05 11:35:00 -0400

    Done.

--- db/migrate/20160705152528_create_dogs.rb
+++ db/migrate/20160705152528_create_dogs.rb
@@ -1,5 +1,6 @@
 class CreateDogs < ActiveRecord::Migration
   def up
+    # up method creates our table with name and breed columns.
     create_table :dogs do |t|
       t.string :name
       t.string :breed
@@ -7,6 +8,20 @@ class CreateDogs < ActiveRecord::Migration
   end
 
   def down
+    # down method drops the table
     drop_table :dogs
   end
 end
+
+#
+# The change method is actually a shorter way of writing up and down methods. We can refactor our migration to look like this:
+# class CreateDogs < ActiveRecord::Migration
+#   def change
+#     create_table :dogs do |t|
+#       t.string :name
+#       t.string :breed
+#     end
+#   end
+#
+# end
+# While the rollback (down) method is not included, it's implicit in the change method. Rolling back the database would work in exactly the same way as using the down
