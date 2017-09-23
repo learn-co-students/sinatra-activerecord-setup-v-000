6bfca24a104bfd70abea87d225e3134fca4bf89a
joanna <github email address>
2017-09-23 15:20:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
+gem 'sinatra'
+gem 'activerecord', '4.2.5'
+gem 'sinatra-activerecord'
+gem 'rake'
+gem 'thin'
+gem 'require_all'
+
+group :development do
+        gem 'shotgun'
+        gem 'pry'
+        gem 'tux'
+        gem 'sqlite3'
+    end
+
 class App < Sinatra::Base
 end
