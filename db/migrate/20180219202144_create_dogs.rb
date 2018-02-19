93ea71b07a1033254a5471a3d5963bc4104c5bac
github username <github email address>
2018-02-19 14:30:10 -0600

    Done.

--- db/migrate/20180219202144_create_dogs.rb
+++ db/migrate/20180219202144_create_dogs.rb
@@ -1,13 +1,19 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
-      t.timestamps
-    end
-  end
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #     t.timestamps
+  #   end
+  # end
+  #
+  # def down
+  #   drop_table :dogs
+  # end
 
-  def down
-    drop_table :dogs
+  def change
+   create_table :dogs do |t|
+     t.string :name
+     t.string :breed
   end
 end
