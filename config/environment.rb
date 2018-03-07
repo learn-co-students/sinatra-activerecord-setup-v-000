3540d75d474abe4adc8e6fe63e1a42343b6d610b
TylerP33 <tyler3321@gmail.com>
2018-03-07 12:23:33 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+  #this is where we set up our database.  The necessary files and folders have not been created yet though.
+  #rake will do that for us
+end
+
 require './app'
