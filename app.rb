c8362a231b5e5212f5adc632d1823cc9db8e2504
Araba Taylor <araba.taylor20@gmail.com>
2018-03-23 00:04:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
+require 'rake'
+require 'sinatra-activerecord'
+require 'activerecord', '4.2.5'
+require 'sqlite3'
+require 'tux'
 
 class App < Sinatra::Base
 end
